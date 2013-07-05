91576483c6bb118ae93c56f2b8987beab1a68b6f
Korcia <jmgmontes@gmail.com>
2013-07-05 18:07:52 +0000

    web.js modified with number of bytes

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
     var lectura = new Buffer(fs.readFileSync('index.html'), "utf-8");
-    response.send(lectura.toString("utf-8",0,27));
+    response.send(lectura.toString("utf-8",0,2801));
 });
 
 var port = process.env.PORT || 5000;
