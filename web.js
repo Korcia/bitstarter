1554cdd6073b0ddc56902408f640788ae7b1f763
Korcia <jmgmontes@gmail.com>
2013-07-05 15:17:48 +0000

    change web.js and added index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
+//var buffer = new Buffer(27);
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    var lectura = new Buffer(fs.readFileSync('index.html'), "utf-8");
+    response.send(lectura.toString("utf-8",0,27));
 });
 
 var port = process.env.PORT || 5000;
